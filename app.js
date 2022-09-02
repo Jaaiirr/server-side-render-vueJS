eb5a4b4a666c594a4c78e59d36520c459dd91a7c
Jair Eduardo Tellez Romero <jairtellezromero9678@gmail.com>
2022-09-02 16:11:36 -0500

    SSR-Vue, without styles

---
+++ app.js
@@ -0,0 +1,12 @@
+//Esto corre en el servidor de Node JS
+import { createSSRApp } from "vue";
+
+// Creamos nuestro componente haciendo uso de ssr para indicar 
+//que el contenido sera cargado del lado del servidor.
+export function createApp() {
+    return createSSRApp({
+      data: () => ({ count: 1 }),
+      template: `<div @click="count++">{{ count }}</div>`,
+    });
+  }
+  
